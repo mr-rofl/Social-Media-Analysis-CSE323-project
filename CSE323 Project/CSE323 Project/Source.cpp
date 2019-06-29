c6ac9ec2d0f5fdf29e3ab7780f76d005137445be
Omar Ibrahim <36420865+mr-rofl@users.noreply.github.com>
2019-06-29 22:32:58 +0200

    added closeness centrality, uncompleted betweeness

--- CSE323 Project/CSE323 Project/Source.cpp
+++ CSE323 Project/CSE323 Project/Source.cpp
@@ -2,50 +2,266 @@
 #include <vector>
 #include <fstream>
 #include <string>
+
+int n,m;
+
+int* betweeness_count;
 using namespace std;
 
+
+
+    
+//degree of centrality//////////
+
+void degree_centrality(){
+	string s;
+	ofstream myfile("graph.txt");
+	vector<int> centrality(n,0); //contains centrality for each node
+	int from,to,dum;
+		
+	for (int i = 0; i < m; i++)
+	{
+		cin>>from>>to>>dum;
+		s=to_string(from)+" "+to_string(to)+" "+to_string(dum)+"\n";
+		myfile<<s; //append to text file
+			
+		centrality[from]++;
+		centrality[to]++;
+	}
+	myfile.close();
+	for (int i = 0; i < n; i++)
+	{
+		cout<<centrality[i];
+		cout<<"\n";
+	}
+}
+
+//end of degree of centrality////////////////
+
+////////////////////////////////////////////////////////////////////////////////////
+
+int minDistance(vector<int> dist, vector<bool> sptSet) /// USED FOR 2 & 3
+{
+    // Initialize min value
+    int min = INT_MAX, min_index;
+
+    for (int v = 0; v < n; v++)
+        if (sptSet[v] == false && dist[v] <= min)
+            min = dist[v], min_index = v;
+
+    return min_index;
+}
+
+///CLOSENESS CENTRALITY//////////
+void printCloseness(vector<int> dist)
+{
+    float sum = 0;
+
+    for (int i = 0; i < n ; i++)
+        sum += dist[i];
+
+    printf("%.7f\n", (n-1)/sum);
+
+}
+
+void dijkstra(vector<vector <int>> graph, int src)
+{
+    vector<int> dist(n);     // The output array.  dist[i] will hold the shortest
+    // distance from src to i
+
+    vector<bool> sptSet(n); // sptSet[i] will be true if vertex i is included in shortest
+    // path tree or shortest distance from src to i is finalized
+
+    // Initialize all distances as INFINITE and stpSet[] as false
+    for (int i = 0; i < n; i++)
+        dist[i] = INT_MAX, sptSet[i] = false;
+
+    // Distance of source vertex from itself is always 0
+    dist[src] = 0;
+
+    // Find shortest path for all vertices
+    for (int count = 0; count < n-1; count++)
+    {
+        // Pick the minimum distance vertex from the set of vertices not
+        // yet processed. u is always equal to src in the first iteration.
+        int u = minDistance(dist, sptSet);
+
+        // Mark the picked vertex as processed
+        sptSet[u] = true;
+
+        // Update dist value of the adjacent vertices of the picked vertex.
+        for (int v = 0; v < n; v++)
+
+            // Update dist[v] only if is not in sptSet, there is an edge from
+            // u to v, and total weight of path from src to  v through u is
+            // smaller than current value of dist[v]
+            if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX
+                && dist[u]+graph[u][v] < dist[v])
+                dist[v] = dist[u] + graph[u][v];
+    }
+
+    // print the constructed distance array
+    printCloseness(dist);
+}
+
+void closeness_centrality(){
+	string s;
+	ofstream myfile("graph.txt");
+	vector<vector<int>> adj_mat(n, vector<int> (n,0)) ;
+	int from,to,dum;
+	for (int i = 0; i < m; i++)
+	{
+		cin>>from>>to>>dum;
+		adj_mat[from][to]=dum;
+        adj_mat[to][from]=dum;
+		s=to_string(from)+" "+to_string(to)+" "+to_string(dum)+"\n";
+		myfile<<s; //append to text file
+	}
+	myfile.close();
+    for (int i = 0; i < n; i++)
+    {
+        dijkstra(adj_mat, i);            
+    }
+}
+///END OF CLOSENESS CENTRALITY///////////
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+///BETWEENESS CENTRALITY///////
+
+void printPath(vector<int> parent, int j) 
+{ 
+      
+    // Base Case : If j is source 
+    if (parent[j] == - 1) 
+        return; 
+  
+    printPath(parent, parent[j]); 
+  
+    printf("%d ", j); 
+	betweeness_count[j]++; /// di eli ana zawedtaha 3ashan ne3ed fiha
+}
+void printSolution(vector<int> dist, int n,  vector<int> parent,int s) 
+{ 
+    int src = s; 
+    for (int i = 1; i < n; i++) 
+    { 
+        printf("\n%d -> %d \t\t %d\t\t%d ", 
+                      src, i, dist[i], src); 
+        printPath(parent, i); 
+    } 
+}
+
+void dijkstra1(vector<vector <int>> graph , int src)
+{ 
+      
+    // The output array. dist[i] 
+    // will hold the shortest 
+    // distance from src to i 
+    vector<int> dist(n);  
+  
+    // sptSet[i] will true if vertex 
+    // i is included / in shortest 
+    // path tree or shortest distance  
+    // from src to i is finalized 
+    vector<bool> sptSet(n); 
+  
+    // Parent array to store 
+    // shortest path tree 
+    vector<int> parent(n); 
+  
+    // Initialize all distances as  
+    // INFINITE and stpSet[] as false 
+    for (int i = 0; i < n; i++) 
+    { 
+        parent[src] = -1; 
+        dist[i] = INT_MAX; 
+        sptSet[i] = false; 
+    } 
+  
+    // Distance of source vertex  
+    // from itself is always 0 
+    dist[src] = 0; 
+  
+    // Find shortest path 
+    // for all vertices 
+    for (int count = 0; count < n - 1; count++) 
+    { 
+        // Pick the minimum distance 
+        // vertex from the set of 
+        // vertices not yet processed.  
+        // u is always equal to src 
+        // in first iteration. 
+        int u = minDistance(dist, sptSet); 
+  
+        // Mark the picked vertex  
+        // as processed 
+        sptSet[u] = true; 
+  
+        // Update dist value of the  
+        // adjacent vertices of the 
+        // picked vertex. 
+        for (int v = 0; v < n; v++) 
+  
+            // Update dist[v] only if is 
+            // not in sptSet, there is 
+            // an edge from u to v, and  
+            // total weight of path from 
+            // src to v through u is smaller 
+            // than current value of 
+            // dist[v] 
+            if (!sptSet[v] && graph[u][v] && 
+                dist[u] + graph[u][v] < dist[v]) 
+            { 
+                parent[v] = u; 
+                dist[v] = dist[u] + graph[u][v]; 
+            }  
+    } 
+  
+    // print the constructed 
+    // distance array 
+    printSolution(dist, n, parent,src); 
+} 
+
+void betweeness_centrality(){
+	string s;
+	ofstream myfile("graph.txt");
+	vector<vector<int>> adj_mat(n, vector<int> (n,0)) ;
+	int from,to,dum;
+	for (int i = 0; i < m; i++)
+	{
+		cin>>from>>to>>dum;
+		adj_mat[from][to]=dum;
+        adj_mat[to][from]=dum;
+		s=to_string(from)+" "+to_string(to)+" "+to_string(dum)+"\n";
+		myfile<<s; //append to text file
+	}
+	myfile.close();
+	/*for (int i = 0; i < n; i++)
+	{
+		dijkstra1(adj_mat,i);
+	}*/
+	dijkstra1(adj_mat,0); /// hatedrab ma3ak law tani argument ay 7aga 8eir 0 
+}
+
+///END OF BETWEENESS CENTRALITY//////////
+
 int main(){
-	int n,m;
 	cin>>n>>m;
 
-	ofstream myfile("graph.txt"); //open file to edit
-	string s;
 	
-	if (n>100) // 1st algorithm
+	betweeness_count = new int[n];
+	for (int i = 0; i < n; i++)
 	{
-		vector<vector<bool>> adj_mat(n, vector<bool> (n,false)) ;
-		vector<int> centrality(n,0); //contains centrality for each node
-		int from,to,dum;
-		
-		for (int i = 0; i < m; i++)
-		{
-			cin>>from>>to>>dum;
-			s=to_string(from)+" "+to_string(to)+" "+to_string(dum)+"\n";
-			myfile<<s; //append to text file
-			adj_mat[from][to]=true; //set the matrix
-			centrality[from]++;
-			centrality[to]++;
-		}
-		myfile.close();
-		for (int i = 0; i < n; i++)
-		{
-			cout<<centrality[i]<<endl;
-		}
-		return 0;
+		betweeness_count[i]=0;
 	}
-	else //2nd or 3rd
-	{
-		vector<vector<int>> adj_mat(n, vector<int> (n,0)) ;
-		int from,to,dum;
-		for (int i = 0; i < m; i++)
-		{
-			cin>>from>>to>>dum;
-			adj_mat[from][to]=dum;
-			s=to_string(from)+" "+to_string(to)+" "+to_string(dum)+"\n";
-			myfile<<s; //append to text file
-		}
-		myfile.close();
-	}
+
+
+	//degree_centrality();
+	//closeness_centrality();
+	betweeness_centrality();
 
 	return 0;
 }
